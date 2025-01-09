5c849f3778d0e3e59d86079674788ff8860a3726
andiez02dev <tan.nguyen@politetech.com>
2025-01-09 11:36:19 +0700

    Feature: create Service layer

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -1,21 +1,20 @@
 import { StatusCodes } from "http-status-codes";
-import ApiError from "~/utils/ApiError";
+import { boardService } from "~/services/boardService";
 
 const createNew = async (req, res, next) => {
   try {
-    console.log("🚀 ~ createNew ~ req.body:", req.body);
-    console.log("🚀 ~ createNew ~ req.query:", req.query);
-    console.log("🚀 ~ createNew ~ req.params:", req.params);
-    console.log("🚀 ~ createNew ~ req.files:", req.files);
-    console.log("🚀 ~ createNew ~ req.cookies:", req.cookies);
-    console.log("🚀 ~ createNew ~ req.jwtDecoded:", req.jwtDecoded);
-    //Điều hướng dữ liệu sang tầng Service
+    // console.log("🚀 ~ createNew ~ req.body:", req.body);
+    // console.log("🚀 ~ createNew ~ req.query:", req.query);
+    // console.log("🚀 ~ createNew ~ req.params:", req.params);
+    // console.log("🚀 ~ createNew ~ req.files:", req.files);
+    // console.log("🚀 ~ createNew ~ req.cookies:", req.cookies);
+    // console.log("🚀 ~ createNew ~ req.jwtDecoded:", req.jwtDecoded);
 
-    throw new ApiError(StatusCodes.BAD_GATEWAY, "Error tesst");
+    //Điều hướng dữ liệu sang tầng Service
+    const createdBoard = await boardService.createNew(req.body);
+
     //Có kết quả =>> trả về Client
-    // res
-    //   .status(StatusCodes.CREATED)
-    //   .json({ message: "POS form Controller: API create list board" });
+    res.status(StatusCodes.CREATED).json(createdBoard);
   } catch (error) {
     next(error);
   }
